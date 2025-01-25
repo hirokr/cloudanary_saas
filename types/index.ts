dd854f157d24e25eee54e64c03662ecf17b7df09
hirokr <hrr3645@gmail.com>
2025-01-25 19:49:01 +0600

    bug fixed

--- types/index.ts
+++ types/index.ts
@@ -3,8 +3,8 @@ export interface Video {
   title: string
   description: string
   publicId: string
-  originalSize: number
-  compressedSize: number
+  originalSize: string
+  compressedSize: string
   duration: number
   createdAt: Date
   updatedAt: Date
